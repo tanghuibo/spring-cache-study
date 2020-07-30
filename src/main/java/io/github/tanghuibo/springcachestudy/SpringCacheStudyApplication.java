bb8697c06214a2b7cef07ca3d5b3412d54771fa0
tang_huibo <1215893264@qq.com>
2020-07-30 23:24:33 +0800

    basic init

---
+++ src/main/java/io/github/tanghuibo/springcachestudy/SpringCacheStudyApplication.java
@@ -0,0 +1,13 @@
+package io.github.tanghuibo.springcachestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCacheStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCacheStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/tanghuibo/springcachestudy/SpringCacheStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tanghuibo.springcachestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCacheStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
